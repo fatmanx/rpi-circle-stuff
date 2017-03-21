0c0ef93f5c56c4cc81d895520a4ed8e114e39c7e
Ionut Oancea <ionut.oancea@onecom.no>
2017-03-21 20:09:36 +0200

    first

---
+++ Raspberry Metal Lib/build.bat
@@ -0,0 +1,4 @@
+PATH=C:\SysGCC\Raspberry\arm-linux-gnueabihf\bin:%PATH%
+PATH=C:\SysGCC\Raspberry\bin;%PATH%
+PATH=C:\Program Files (x86)\GnuWin32\bin;%PATH%
+make
---
+++ Raspberry Metal Lib/clean.bat
@@ -0,0 +1,3 @@
+PATH=C:\SysGCC\Raspberry\arm-linux-gnueabihf\bin:%PATH%
+PATH=C:\SysGCC\Raspberry\bin;%PATH%
+make clean
